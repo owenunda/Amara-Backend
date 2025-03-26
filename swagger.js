bcc37b663e14effe164f89dd892a25dcbc8cfe55
oweenunda <oweenunda.dev@gmail.com>
2025-03-26 08:10:35 -0500

    fix: bug in url

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
     title: 'API para el sistema de inventario de AMARA SAS',
     description: 'Esta API permite gestionar todo el sistema de invitenario de la produccion de quesos de la empresa AMARA SAS'
   },
-  host: 'https://amara-backend-production.up.railway.app',
+  host: 'amara-backend-production.up.railway.app',
   schema: ['https']
 }
 swaggerAutogen()(outpuFile, endPointsFiles, doc)
