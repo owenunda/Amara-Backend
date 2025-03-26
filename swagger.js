1ad2395e689552afa9464477d13083fb8989d063
oweenunda <oweenunda.dev@gmail.com>
2025-03-26 11:03:19 -0500

    despliegue para railway

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
     title: 'API para el sistema de inventario de AMARA SAS',
     description: 'Esta API permite gestionar todo el sistema de invitenario de la produccion de quesos de la empresa AMARA SAS'
   },
-  host: 'amara-backend.onrender.com',
-  schema: ['https']
+  host: 'amara-backend-production.up.railway.app',
+  schemes: ['https']
 }
 swaggerAutogen()(outpuFile, endPointsFiles, doc)
