eb2fa8b2292156e587e811545fa072660292320a
owenunda <oweenunda.dev@gmail.com>
2025-04-14 15:55:49 -0500

    add: creado el endpoint de eliminar personas

--- src/services/persona.service.js
+++ src/services/persona.service.js
@@ -8,6 +8,10 @@ class PersonaService {
   static async obtenerPersonaPorId (id) {
     return await PersonaDao.obtenerPersonaPorId(id)
   }
+
+  static async eliminarPersona (id) {
+    return await PersonaDao.eliminarPersona(id)
+  }
 }
 
 export default PersonaService
