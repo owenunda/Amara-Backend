39e6b3459a71b8ff9b7caaedcfef8f99636b3e64
oweenunda <oweenunda.dev@gmail.com>
2025-05-09 02:15:37 -0500

    proteccion de rutas

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -9,6 +9,7 @@ import compraRouters from '../controllers/compra.controller.js'
 import quesosRouters from '../controllers/quesos.controller.js'
 import ProduccionRouter from '../controllers/produccion.controller.js'
 import ventaRouter from '../controllers/venta.controller.js'
+
 const router = express.Router()
 
 router.get('/perfil', authMiddleware, (req, res) => {
@@ -23,7 +24,7 @@ router.use('/proveedor', proveedorRouters)
 router.use('/materia-prima', materiaPrimaRouters)
 router.use('/compra', compraRouters)
 router.use('/queso', quesosRouters)
-router.use('/produccion', ProduccionRouter)
+router.use('/produccion', authMiddleware, ProduccionRouter)
 router.use('/venta', ventaRouter)
 
 export default router
